b3b10872ad6efa48cdc646beba419ddb018e7692
Hazel Eythorsdottir <hazeleythorsdottir@gmail.com>
2022-10-03 16:43:34 -0500

    Update for dist upgrade

--- src/RogEng/logging.cpp
+++ src/RogEng/logging.cpp
@@ -1,22 +1,43 @@
 #include "logging.hpp"
-#include <cstdio>
 #include <cstdarg>
+#include <cstdio>
 #include <filesystem>
+#include <fstream>
+#include <string>
+#include <ctime>
 
-const char* LogLevelNames[] = { "DEBUG", "INFO", "WARNING", "FATAL" }; 
+namespace {
+bool           LogToFile = false;
+RE::Log::Level MinLevel = RE::Log::DEBUG;
+const char*    LogLevelNames[] = {"DEBUG", "INFO", "WARNING", "FATAL"};
+std::ofstream  LogFile;
+} // namespace
 
+const char* LogLevelToName_(RE::Log::Level level) {
+  return LogLevelNames[level];
+}
 std::string GetFileName_(const char* path) {
   return std::filesystem::path(path).filename().string();
 };
-
-const char* LogLevelToName_(RE_::LogLevel level) {
-  return LogLevelNames[level];
-}
-
-void RE_::Log (RE_::LogLevel level, const char* source, int linenum, const char* fmt, ...) {
+void RE::Log::SetLogToFile(bool LogToFile) { ::LogToFile = LogToFile; };
+void RE::Log::SetMinLogLevel(RE::Log::Level MinLevel) {
+  ::MinLevel = MinLevel;
+};
+void RE_::Log::Log(RE::Log::Level level, const char* source, int linenum,
+                   const char* fmt, ...) {
+  if (level < MinLevel) {
+    return;
+  }
   va_list args;
   va_start(args, fmt);
-  std::printf("[ %s %s:%d ] ", LogLevelToName_(level), GetFileName_(source).c_str(), linenum);
+  std::printf("[ %s %s:%d ] ", LogLevelToName_(level),
+              GetFileName_(source).c_str(), linenum);
   std::vprintf(fmt, args);
   va_end(args);
+}
+int RE_::Log::Initialize() { 
+  std::time_t t = std::time(0);
+  std::tm* now = std::localtime(&t);
+  LogFile = std::ofstream("log-");
+  return 0; 
 }
--- src/RogEng/logging.hpp
+++ src/RogEng/logging.hpp
@@ -1,20 +1,32 @@
 #pragma once
 
+namespace RE {
+namespace Log {
+enum Level { DEBUG = 0, INFO, WARNING, FATAL };
+void SetLogToFile(bool LogToFile);
+void SetLogToStdout(bool LogToStdout);
+void SetMinLogLevel(RE::Log::Level MinLevel);
+} // namespace Log
+} // namespace RE
+
 namespace RE_ {
-enum LogLevel { DEBUG = 0, INFO, WARNING, FATAL };
+namespace Log {
 // You probably won't need to use this, you should most likely use a define
 // Formats with vsprintf
-void Log(LogLevel    level,
-         const char* source,  // File source, use __FILE__
-         int         linenum, // Line number, use __LINE__
-         const char* fmt, ...);
+void Log(RE::Log::Level level,
+         const char*    source,  // File source, usually __FILE__
+         int            linenum, // Line number, usually __LINE__
+         const char*    fmt, ...);
+int  Initialize();
+void Exit();
+} // namespace Log
 } // namespace RE_
 
 #define RE__LOG_DEBUG(...)                                                     \
-  RE_::Log(RE_::LogLevel::DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+  RE_::Log::Log(RE::Log::Level::DEBUG, __FILE__, __LINE__, __VA_ARGS__)
 #define RE__LOG_INFO(...)                                                      \
-  RE_::Log(RE_::LogLevel::INFO, __FILE__, __LINE__, __VA_ARGS__)
+  RE_::Log::Log(RE::Log::Level::INFO, __FILE__, __LINE__, __VA_ARGS__)
 #define RE__LOG_WARNING(...)                                                   \
-  RE_::Log(RE_::LogLevel::WARNING, __FILE__, __LINE__, __VA_ARGS__)
+  RE_::Log::Log(RE::Log::Level::WARNING, __FILE__, __LINE__, __VA_ARGS__)
 #define RE__LOG_FATAL(...)                                                     \
-  RE_::Log(RE_::LogLevel::FATAL, __FILE__, __LINE__, __VA_ARGS__)
+  RE_::Log::Log(RE::Log::Level::FATAL, __FILE__, __LINE__, __VA_ARGS__)
